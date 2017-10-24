a455b06f45d58b57f68116ccfa46d444d9f5b49a
Ángel Andrade García <a01375834@DPDMASTER-5.local>
2017-10-24 14:22:35 -0500

    Ahora si ya quedo el vigilante

--- VisitMe/Vigilante.swift
+++ VisitMe/Vigilante.swift
@@ -7,3 +7,58 @@
 //
 
 import Foundation
+
+
+class Vigilante{
+    
+    var id: String?
+    var nombre: String?
+    var apellidoPaterno: String?
+    var apellidoMaterno: String?
+    
+    init(id: String, nombre:String, apellidoPaterno:String, apellidoMaterno:String){
+        self.id = id
+        self.nombre = nombre
+        self.apellidoPaterno = apellidoPaterno
+        self.apellidoMaterno = apellidoMaterno
+        
+        func getId() -> String{
+            return self.id!
+        }
+        
+        func getNombre() -> String{
+            return self.nombre!
+        }
+        
+        func getApellidoPaterno() -> String{
+            return self.apellidoPaterno!
+        }
+        
+        func getApellidoMaterno() -> String{
+            return self.apellidoPaterno!
+            
+        }
+        
+        func setId(id: String){
+            self.id = id
+        }
+        
+        func setNombre(nombre: String){
+            self.nombre = nombre
+        }
+        
+        func setApellidoPaterno(apellidoPaterno: String){
+            self.apellidoPaterno = apellidoPaterno
+        }
+        
+        func setApellidoMaterno(apellidoMaterno: String){
+            self.apellidoMaterno = apellidoMaterno
+        }
+        
+        
+    }
+    
+    
+
+    
+}
