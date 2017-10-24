cd3f57f5723f4fd75feb06a77738591c467ebcb0
Ángel Andrade García <a01375834@DPDMASTER-5.local>
2017-10-24 14:17:14 -0500

    clase vigilante creada

---
+++ VisitMe/Vigilante.swift
@@ -0,0 +1,9 @@
+//
+//  Vigilante.swift
+//  VisitMe
+//
+//  Created by Ángel Andrade García on 24/10/17.
+//  Copyright © 2017 Oscar Allan Ruiz Toledo . All rights reserved.
+//
+
+import Foundation
