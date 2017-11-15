1d67334a34a7728e7c9f2a344c252431ba5c7bda
oscarruiz <oscar.ruiz@konfio.mx>
2017-11-15 02:00:24 -0600

    Controllers para el TableView y la pantalla de registro exitoso

---
+++ VisitMe/Controller/InvitadoRegistradoController.swift
@@ -0,0 +1,9 @@
+//
+//  InvitadoRegistradoController.swift
+//  VisitMe
+//
+//  Created by Oscar Allan Ruiz Toledo  on 14/11/17.
+//  Copyright © 2017 Oscar Allan Ruiz Toledo . All rights reserved.
+//
+
+import Foundation
---
+++ VisitMe/Controller/ListaVisitantesController.swift
@@ -0,0 +1,9 @@
+//
+//  ListaVisitantesController.swift
+//  VisitMe
+//
+//  Created by Oscar Allan Ruiz Toledo  on 15/11/17.
+//  Copyright © 2017 Oscar Allan Ruiz Toledo . All rights reserved.
+//
+
+import Foundation
